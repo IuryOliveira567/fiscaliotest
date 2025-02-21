862046a759e1d4b46fcd4294e7218c68ddd69d72
IuryOliveira567 <Iury.Oliveira567@gmail.com>
2025-02-21 10:33:59 -0300

    feat: add tela de exibicao de nota fiscal

---
+++ src/view/NotaFiscalTabela.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="nota-fiscal-tabela">Nota fiscal</div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'NotaFiscalTabela'
+  })
+</script>
---
+++ src/view/index.ts
@@ -0,0 +1,1 @@
+export { default as NotaFiscalTabela } from './NotaFiscalTabela.vue';
