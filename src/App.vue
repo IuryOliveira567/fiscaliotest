f2f0db8790790beb281de0de0fa16fc583939a05
IuryOliveira567 <Iury.Oliveira567@gmail.com>
2025-02-21 10:34:42 -0300

    feat: tela de nota fiscal

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  <div></div>
+  <div class="main-container">
+    <NotaFiscalTabela/>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { NotaFiscalTabela } from './view';
 
 export default defineComponent({
   name: 'App',
   components: {
+    NotaFiscalTabela
   }
 });
 </script>
