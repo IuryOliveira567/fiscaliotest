83df32ab859a397f9c5a4de8c0416372d556890b
IuryOliveira567 <Iury.Oliveira567@gmail.com>
2025-03-06 11:58:08 -0300

    feat: interface modelo do componente da tabela

---
+++ src/model/TabelaModel.ts
@@ -0,0 +1,26 @@
+import NotaFiscalItem from "./NotaFiscalItem"
+
+interface TableColumn {
+  label: string,
+  field: string,
+  width: string,
+  sortable: boolean,
+  isKey: boolean
+}
+
+type SortableType = "asc" | "desc";
+
+interface SortableConfig {
+  order: string,
+  sort: SortableType
+}
+
+interface TabelaModelData {
+  isLoading: boolean,
+  columns: TableColumn[],
+  rows: NotaFiscalItem[],
+  totalRecordCount: number,
+  sortable: SortableConfig
+}
+
+export default TabelaModelData;
