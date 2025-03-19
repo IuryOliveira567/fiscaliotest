a5a81228b37afdb7f9034b6a7fd396fafdf87504
IuryOliveira567 <Iury.Oliveira567@gmail.com>
2025-03-19 12:32:00 -0300

    feat: formulário de edição de nota fiscal

---
+++ src/components/NotaFiscalForm.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="nota-fiscal-form-modal">
+    <div class="nota-fiscal-form">
+      <form class="form-control">
+        <h1>Editar Emissor</h1>
+        <div>
+          <div class="row">
+            <div class="col-md-6">
+              <label for="id-nota" class="form-label">ID: </label>
+              <input v-model="dadosNotaFiscal.idnota" type="number" class="form-control" readonly />
+            </div>
+            <div class="col-md-6">
+              <label for="id-data" class="form-label">DATA: </label>
+              <input v-model="dadosNotaFiscal.data" type="text" class="form-control" readonly />
+            </div>
+          </div>
+          <div class="row">
+            <label>EMISSOR: </label>
+            <input v-model="dadosNotaFiscal.emissor" type="text" class="form-control" />
+          </div>
+        </div>
+        <div>
+          <button class="btn btn-success btn-lg" type="button">Salvar</button>
+          <button @click="$emit('fecha-modal')" class="btn btn-primary btn-lg" type="button">Fechar</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType  } from 'vue';
+import { NotaFiscalItem } from '@/model';
+
+
+export default defineComponent({
+  name: 'NotaFiscalForm',
+  props: {
+    notaFiscalData: {
+      type: Object as PropType<NotaFiscalItem>,
+      required: true
+    }
+  },
+  created() {
+    this.dadosNotaFiscal = this.notaFiscalData
+    this.dadosNotaFiscal.data = this.formatarData(this.dadosNotaFiscal.data);
+  },
+  data() {
+    return {
+      dadosNotaFiscal: {} as NotaFiscalItem
+    }
+  },
+  methods: {
+    formatarData(data: string) {
+      return new Date(data).toISOString().split("T")[0];
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+  .nota-fiscal-form-modal {
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    z-index: 1050 !important;
+    background-color: rgba(0, 0, 0, 0.4);
+    pointer-events: auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+
+    .nota-fiscal-form {
+      width: 80vw;
+      height: 60vh;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      z-index: 1055 !important;
+      pointer-events: auto;
+
+      h1 {
+        margin-top: 0.6rem;
+      }
+
+      form {
+        width: 80%;
+        height: 80%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-around;
+        border-radius: 20px;
+
+        input {
+          margin-bottom: 2rem;
+        }
+      }
+
+      button {
+        margin-left: 1rem;
+      }
+    }
+  }
+</style>
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as NotaFiscalForm } from './NotaFiscalForm.vue';
