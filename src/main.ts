e67423eddedeafc89d01778410a4638f0f024f5d
IuryOliveira567 <Iury.Oliveira567@gmail.com>
2025-03-19 10:20:32 -0300

    build: bootstrap vue 2.23.1

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,7 @@ import App from './App.vue'
 
 localStorage.setItem('REMOTE_API_URL', 'http://127.0.0.1:5000/notasfiscais');
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
 createApp(App).mount('#app');
