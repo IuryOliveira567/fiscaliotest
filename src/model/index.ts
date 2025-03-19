dac9c22b3b574a45b757dd9745a5c9195287a11c
IuryOliveira567 <Iury.Oliveira567@gmail.com>
2025-03-19 10:08:21 -0300

    refact: correção de importação de modulos

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,4 +1,5 @@
 export { default as NotaFiscalItem } from './NotaFiscalItem';
 export { default as BuscaNotasFiscaisModel } from './BuscaNotasFiscaisModel';
-export { default as BuscaNotasFiscais } from './BuscaNotasFiscais';
 export { default as TabelaModel } from './TabelaModel';
+export { default as ApiParams } from './ApiParams';
+export { default as GerenciaNotasFiscais } from './GerenciaNotasFiscaisModel';
